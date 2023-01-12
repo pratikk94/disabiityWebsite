422821c65361ea923448251d2e3ed88ee012ff5f
user name <user email>
2023-01-12 10:16:15 +0530

    Schemes added

--- src/components/scheme.jsx
+++ src/components/scheme.jsx
@@ -1,38 +1,117 @@
 import { regular } from "../Responsive/constants";
 import parse from "html-react-parser";
 
-export default function Scheme({ rawJsonData, selectdType }) {
-  if (selectdType.includes("educational")) {
-    let jsonData = rawJsonData["schemes"][4];
-    if (jsonData !== undefined) {
-      return (
-        <div>
-          <h1
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "60%",
-              whiteSpace: "normal",
-              color: "#1a2c6d",
-              marginTop: "2%",
-            }}
-          >
-            {jsonData["nameOfScheme"]}
-          </h1>
+export default function Scheme(props) {
+  //if (props.selectdType.includes("educational")) {
+  if (props.rawJsonData !== undefined) {
+    console.log(props.rawJsonData);
+    return props.rawJsonData["schemes"].map((jsonData, index) => (
+      <div>
+        <h1
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "60%",
+            whiteSpace: "normal",
+            color: "#1a2c6d",
+            marginTop: "2%",
+          }}
+        >
+          {jsonData["nameOfScheme"]}
+        </h1>
 
-          <h1
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "62%",
-              whiteSpace: "normal",
-              color: "#1a2c6d",
-              marginTop: "1%",
-            }}
-          >
-            Entitlements
-          </h1>
-          <p
+        <h1
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "62%",
+            whiteSpace: "normal",
+            color: "#1a2c6d",
+            marginTop: "1%",
+          }}
+        >
+          Entitlements
+        </h1>
+        <p
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "60%",
+            marginLeft: "4%",
+            fontSize: regular.fontSizeText,
+            whiteSpace: "normal",
+          }}
+        >
+          <pre style={{ whiteSpace: "pre-wrap" }}>
+            {parse(jsonData["entitlements"])}
+          </pre>
+        </p>
+        <h1
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "62%",
+            whiteSpace: "normal",
+            color: "#1a2c6d",
+            marginTop: "1%",
+          }}
+        >
+          Eligibility Requirments
+        </h1>
+        <p
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "60%",
+            marginLeft: "4%",
+            fontSize: regular.fontSizeText,
+            whiteSpace: "normal",
+          }}
+        >
+          <pre style={{ whiteSpace: "pre-wrap" }}>
+            {parse(jsonData["eligibilityRequirement"])}
+          </pre>
+        </p>
+        <h1
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "62%",
+            whiteSpace: "normal",
+            color: "#1a2c6d",
+            marginTop: "1%",
+          }}
+        >
+          Documents required
+        </h1>
+        <p
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "60%",
+            marginLeft: "4%",
+            fontSize: regular.fontSizeText,
+            whiteSpace: "normal",
+          }}
+        >
+          <pre style={{ whiteSpace: "pre-wrap" }}>
+            {parse(jsonData["documentRequired"])}
+          </pre>
+        </p>
+        <h1
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "62%",
+            whiteSpace: "normal",
+            color: "#1a2c6d",
+            marginTop: "1%",
+          }}
+        >
+          Timelines
+        </h1>
+        <p>
+          <pre
             style={{
               textAlign: "left",
               wordWrap: "break-all",
@@ -42,119 +121,40 @@ export default function Scheme({ rawJsonData, selectdType }) {
               whiteSpace: "normal",
             }}
           >
-            <pre style={{ whiteSpace: "pre-wrap" }}>
-              {parse(jsonData["entitlements"])}
-            </pre>
-          </p>
-          <h1
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "62%",
-              whiteSpace: "normal",
-              color: "#1a2c6d",
-              marginTop: "1%",
-            }}
-          >
-            Eligibility Requirments
-          </h1>
-          <p
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "60%",
-              marginLeft: "4%",
-              fontSize: regular.fontSizeText,
-              whiteSpace: "normal",
-            }}
-          >
-            <pre style={{ whiteSpace: "pre-wrap" }}>
-              {parse(jsonData["eligibilityRequirement"])}
-            </pre>
-          </p>
-          <h1
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "62%",
-              whiteSpace: "normal",
-              color: "#1a2c6d",
-              marginTop: "1%",
-            }}
-          >
-            Documents required
-          </h1>
-          <p
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "60%",
-              marginLeft: "4%",
-              fontSize: regular.fontSizeText,
-              whiteSpace: "normal",
-            }}
-          >
-            <pre style={{ whiteSpace: "pre-wrap" }}>
-              {parse(jsonData["documentRequired"])}
-            </pre>
-          </p>
-          <h1
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "62%",
-              whiteSpace: "normal",
-              color: "#1a2c6d",
-              marginTop: "1%",
-            }}
-          >
-            Timelines
-          </h1>
-          <p>
-            <pre
-              style={{
-                textAlign: "left",
-                wordWrap: "break-all",
-                width: "60%",
-                marginLeft: "4%",
-                fontSize: regular.fontSizeText,
-                whiteSpace: "normal",
-              }}
-            >
-              {parse(jsonData["timeline"])}
-            </pre>
-          </p>
+            {parse(jsonData["timeline"])}
+          </pre>
+        </p>
 
-          <h1
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "62%",
-              whiteSpace: "normal",
-              color: "#1a2c6d",
-              marginTop: "1%",
-            }}
-          >
-            Grievance redressal board
-          </h1>
-          <p
-            style={{
-              textAlign: "left",
-              wordWrap: "break-all",
-              width: "60%",
-              marginLeft: "4%",
-              fontSize: regular.fontSizeText,
-              whiteSpace: "normal",
-            }}
-          >
-            <pre style={{ whiteSpace: "pre-wrap" }}>
-              {parse(jsonData["grievanceRedressalBoard"])}
-            </pre>
-          </p>
-        </div>
-      );
-    } else {
-      return;
-    }
+        <h1
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "62%",
+            whiteSpace: "normal",
+            color: "#1a2c6d",
+            marginTop: "1%",
+          }}
+        >
+          Grievance redressal board
+        </h1>
+        <p
+          style={{
+            textAlign: "left",
+            wordWrap: "break-all",
+            width: "60%",
+            marginLeft: "4%",
+            fontSize: regular.fontSizeText,
+            whiteSpace: "normal",
+          }}
+        >
+          <pre style={{ whiteSpace: "pre-wrap" }}>
+            {parse(jsonData["grievanceRedressalBoard"])}
+          </pre>
+        </p>
+      </div>
+    ));
+  } else {
+    return;
   }
+  //}
 }
